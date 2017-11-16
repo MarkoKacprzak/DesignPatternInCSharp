08ef1602dbc4d2e83ab730f11b1862483640d2d9
Marek.Kacprzak <marek.kacprzak@windowslive.com>
2017-11-16 09:55:54 +0100

    Imutable Builder

--- Substitution/Builders/Person/PersonBuilder.cs
+++ Substitution/Builders/Person/PersonBuilder.cs
@@ -9,40 +9,48 @@ namespace Substitution.Builders.Person
 {
     public class PersonBuilder: IFirstNameHolder, ILastNameHolder, IPrimaryContactHolder, IContanctHolder, IPersonBuilder
     {
-        private string FirstName { get; set; }
-        private string LastName { get; set; }
-        private IContactInfo PrimaryContact { get; set; }
-        public IList<IContactInfo> Contacts { get; set; } = new List<IContactInfo>(); 
+        private string FirstName { get;  }
+        private string LastName { get; }
+        private IContactInfo PrimaryContact { get; }
+        public IList<IContactInfo> Contacts { get; }  
         public static IFirstNameHolder Person()
             => new PersonBuilder();
-        public ILastNameHolder WithFirstName(string firstName)
-            => new PersonBuilder
-            {
-                FirstName = firstName
-            };
-        public IPrimaryContactHolder WithLastName(string lastName)
-            => new PersonBuilder
-            {
-                FirstName = this.FirstName,
-                LastName = lastName
-            };
-        public IContanctHolder WithSecondaryContact(IContactInfo contact)
-        => new PersonBuilder
+        public PersonBuilder()
         {
-            FirstName = this.FirstName,
-            LastName = this.LastName,
-            Contacts = new List<IContactInfo>(Contacts) { contact },
-            PrimaryContact = this.PrimaryContact
-        };
+        }
+        public PersonBuilder(string firstName) : this(firstName, null)
+        {
+        }
+        public PersonBuilder(string firstName,string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Contacts= new List<IContactInfo>();
+        }
+        public PersonBuilder(string firstName, string lastName, IList<IContactInfo> contacts, IContactInfo primary)
+        {
+            Contacts = new List<IContactInfo>(contacts) { primary };
+            PrimaryContact = primary;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public PersonBuilder(string firstName, string lastName,  IList<IContactInfo> contacts, IContactInfo primary, IContactInfo secondary)
+        {
+            Contacts = new List<IContactInfo>(contacts) { secondary };
+            FirstName = firstName;
+            LastName = lastName;
+            PrimaryContact = primary;
+        }
+
+        public ILastNameHolder WithFirstName(string firstName)
+            => new PersonBuilder(firstName);
+        public IPrimaryContactHolder WithLastName(string lastName)
+            => new PersonBuilder(this.FirstName, lastName);
+        public IContanctHolder WithSecondaryContact(IContactInfo contact)
+        => new PersonBuilder(FirstName, LastName, Contacts,PrimaryContact, contact);
 
         public IContanctHolder WithPrimaryContact(IContactInfo contact)
-        => new PersonBuilder
-        {
-            FirstName = this.FirstName,
-            LastName = this.LastName,
-            Contacts = new List<IContactInfo>(this.Contacts) { contact },
-            PrimaryContact = contact
-        };
+        => new PersonBuilder(FirstName, LastName, Contacts, contact);
         public IPersonBuilder AndNoMoreContacts()
             => this;
 
